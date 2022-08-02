d6b99f6af59dcf0fdbcb34559fd6ded96bd08c5a
job karani <jobkarani084j@gmail.com>
2022-08-02 10:17:00 +0300

    creates category class

---
+++ Bethany's pie shop mvc/Models/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace Bethany_s_pie_shop_mvc.Models
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public List<Pie>? Pies { get; set; }
+    }
+}
