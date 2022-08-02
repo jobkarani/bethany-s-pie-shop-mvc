668d5f90d4c8412e16ba7f0c99bf0f6bfc9f6833
job karani <jobkarani084j@gmail.com>
2022-08-02 10:43:57 +0300

    creates a mock category repository

---
+++ Bethany's pie shop mvc/Models/MockCategoryRepository.cs
@@ -0,0 +1,22 @@
+﻿namespace Bethany_s_pie_shop_mvc.Models
+{
+    public class MockCategoryRepository : ICategoryRepository
+    {
+        public IEnumerable<Category> AllCategories =>
+            new List<Category>
+            {
+                new Category
+                {
+                    CategoryId = 1, CategoryName = "Fruit Pies", Description = "All - Fruit Pies"
+                },
+                new Category
+                {
+                    CategoryId = 2, CategoryName = "Cheese Cakes", Description = "Cheesy All The Way!!"
+                },
+                 new Category
+                {
+                    CategoryId = 3, CategoryName = "Seasonal Pies", Description = "Get in the mood for a seasonal pie!!!"
+                }
+            };
+    }
+}
