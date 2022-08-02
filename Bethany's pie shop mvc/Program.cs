56ca9a4e099c5cbaffbf4b73d403007fc5e03a3c
job karani <jobkarani084j@gmail.com>
2022-08-02 11:09:42 +0300

    registered the created repositories in program.cs file

--- Bethany's pie shop mvc/Program.cs
+++ Bethany's pie shop mvc/Program.cs
@@ -1,4 +1,9 @@
+using Bethany_s_pie_shop_mvc.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<ICategoryRepository, MockCategoryRepository>();
+builder.Services.AddScoped<IPieRepository, MockPieRepository>();
 
 builder.Services.AddControllersWithViews();
 
