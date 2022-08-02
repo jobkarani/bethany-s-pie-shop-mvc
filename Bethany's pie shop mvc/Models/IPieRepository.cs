c53097cb2ba9e63a6416bb1e75caa30d010d8754
job karani <jobkarani084j@gmail.com>
2022-08-02 10:28:04 +0300

    creates pie repository

---
+++ Bethany's pie shop mvc/Models/IPieRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace Bethany_s_pie_shop_mvc.Models
+{
+    public interface IPieRepository
+    {
+        IEnumerable<Pie> AllPies { get; }
+        IEnumerable<Pie> PiesOfTheWeek { get; }
+        Pie? GetPieById(int pieId);
+    }
+}
