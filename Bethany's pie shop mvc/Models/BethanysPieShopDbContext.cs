6d3fda60268064260517c79d4e38bf1ed3da1032
job karani <jobkarani084j@gmail.com>
2022-08-05 12:32:29 +0300

    creates bethanyspieshopdbcontext class

---
+++ Bethany's pie shop mvc/Models/BethanysPieShopDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Bethany_s_pie_shop_mvc.Models
+{
+    public class BethanysPieShopDbContext: DbContext
+    {
+        public BethanysPieShopDbContext(DbContextOptions<BethanysPieShopDbContext>options)
+            : base(options) { }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Pie> Pies { get; set; }
+    }
+}
